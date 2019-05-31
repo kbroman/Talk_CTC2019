b3a49282d64ae5e45f565c93d61a852b19433ac8
Karl Broman <kbroman@gmail.com>
2019-05-31 08:15:33 -0500

    min vs self: make points smaller; delete unused ccode

--- R/min_v_self.R
+++ R/min_v_self.R
@@ -37,71 +37,8 @@ grayplot(self, best, bg=color,
          xlab="Proportion discordant with self", ylab="Minimum proportion discordant",
          yat=c(0, 0.05, 0.10, 0.15),
          ylim=c(0, 0.15), xlim=c(0, 0.23), xaxs="i", yaxs="i",
-         mgp.x=c(1.4,0.3,0), mgp.y=c(2.5,0.3,0))
+         mgp.x=c(1.4,0.3,0), mgp.y=c(2.5,0.3,0), cex=0.7)
 
-points(self[mixture], best[mixture], bg=color[mixture], pch=21)
-
-annot <- names(self)[!is.na(self) & self > 0.05]
-
-left <- paste0("DO-", c(205, 174, 397, 385, 336, 142, 186, 191))
-special <- paste0("DO-", c(343, 359, 362, 370, 360, 88, 53, 83, 54, 327, 85))
-right <- annot[!(annot %in% c(left, special))]
-
-text_cex <- 0.75
-
-xd <- 0.002
-#text(self[left] - xd, best[left], left, adj=1, cex=text_cex)
-#text(self[right] + xd, best[right], right, adj=0, cex=text_cex)
-
-# locations for special points
-line_offset <- rbind("DO-343" = c(-0.005, -0.005),
-                     "DO-359" = c(-0.005,  0.003),
-                     "DO-362" = c(-0.001,  0.008),
-                     "DO-53"  = c( 0.003, -0.002),
-                     "DO-54"  = c( 0.003,  0.0025),
-                     "DO-83"  = c( 0.008,  0.007),
-                     "DO-88"  = c( 0.014,  0.014),
-                     "DO-370" = c(-0.004, -0.001),
-                     "DO-85"  = c(-0.004,  0.001),
-                     "DO-360" = c(-0.004,  0.007),
-                     "DO-111" = c(-0.004,  0.003),
-                     "DO-324" = c( 0.004, -0.003),
-                     "DO-327" = c( 0.003,  0.005))
-text_offset <- rbind("DO-343" = c(-0.005, -0.005),
-                     "DO-359" = c(-0.006,  0.002),
-                     "DO-362" = c(-0.007,  0.009),
-                     "DO-53"  = c( 0.003, -0.002),
-                     "DO-54"  = c( 0.003,  0.003),
-                     "DO-83"  = c( 0.008,  0.008),
-                     "DO-88"  = c( 0.014,  0.014),
-                     "DO-370" = c(-0.005, -0.0017),
-                     "DO-85"  = c(-0.005,  0.0017),
-                     "DO-360" = c(-0.004,  0.0075),
-                     "DO-111" = c(-0.004,  0.003),
-                     "DO-324" = c( 0.004, -0.003),
-                     "DO-327" = c( 0.003,  0.005))
-text_adj <- rbind("DO-343"=c(1,  1),
-                  "DO-359"=c(1,  0),
-                  "DO-362"=c(0.5,0),
-                  "DO-53" =c(0,0.5),
-                  "DO-54" =c(0,0.5),
-                  "DO-83" =c(0,0.5),
-                  "DO-88" =c(0,0.5),
-                  "DO-370"=c(1,0.5),
-                  "DO-85" =c(1,0.5),
-                  "DO-360"=c(1,0),
-                  "DO-111"=c(1,0.5),
-                  "DO-324"=c(0,0.5),
-                  "DO-327"=c(0,0))
-
-#for(i in 1:nrow(line_offset)) {
-#    n <- rownames(line_offset)[i]
-#    segments(self[n], best[n], self[n] + line_offset[n,1], best[n] + line_offset[n,2])
-#    points(self[n], best[n], pch=21, bg=color[n])
-#    text(self[n] + text_offset[n,1], best[n] + text_offset[n,2], n, adj=text_adj[n,], cex=text_cex)
-#}
-
-#legend("topleft", pch=21, pt.bg=the_colors,
-#       c("okay", "low reads", "bad DNA", "mixture", "sample mix-up"), bg="gray92")
+points(self[mixture], best[mixture], bg=color[mixture], pch=21, cex=0.7)
 
 dev.off()
